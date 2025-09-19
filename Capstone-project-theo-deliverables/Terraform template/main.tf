a3bb77d4fc2bf648d0aa62900b0f65f1009991fb
tbopoku1 <theophilusopokuboakye@gmail.com>
2025-09-19 16:06:12 +0000

    Update main.tf

--- Capstone-project-theo-deliverables/Terraform template/main.tf
+++ Capstone-project-theo-deliverables/Terraform template/main.tf
@@ -12,13 +12,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# S3 Bucket (Frontend + Audio)
 module "s3" {
   source      = "./modules/s3"
   bucket_name = "theo123-text-to-speech"
 }
 
-# Lambda function
 module "lambda" {
   source            = "./modules/lambda"
   bucket_name       = module.s3.bucket_name
@@ -26,8 +24,8 @@ module "lambda" {
   lambda_source_dir = "/Terraform/lambda/lambda.zip"
 }
 
-# API Gateway
 module "api" {
   source     = "./modules/api"
   lambda_arn = module.lambda.lambda_arn
 }
+
